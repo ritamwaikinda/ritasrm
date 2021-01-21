46335b17515f126a2567a95616e83d76a395e786
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-01-20 23:54:30 -0500

    layout skeleton

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Main from "./components/Main";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import SocialLinks from "./components/SocialLinks";
+import Footer from "./components/Footer";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Home />
+			<Main />
+			<Projects />
+			<Resume />
+			<Contact />
+			<SocialLinks />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
