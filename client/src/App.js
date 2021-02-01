2bf0654ea33cbf3a11ca26423f08cd44c3e149c0
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-01-31 23:52:34 -0500

    Backstage files 01

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,49 @@
-import React /*, { Component }*/ from "react";
-// import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-// import Main from "./components/Main";
-// import Projects from "./components/Projects";
-// import Resume from "./components/Resume";
-import Contact from "./pages/Contact";
-// import SocialLinks from "./components/SocialLinks";
-// import Footer from "./components/Footer";
-import "./App.css";
+import React /*, { Component }*/ from 'react';
+import Ready from './backstagedemo/Ready.jsx';
+import './App.css';
 
 function App() {
 	return (
 		<div>
-			<Home />
-			<Services />
-			<Skills />
-			<Projects />
-			{/* <Navbar />
-			<Home />
-			<Main />
-			<Projects />
-			<Resume />
-			<Contact />
-			<SocialLinks />
-			<Footer /> */}
-			<Contact />
+			<Ready />
 		</div>
 	);
 }
 
 export default App;
+
+// import React /*, { Component }*/ from "react";
+// // import Navbar from "./components/Navbar";
+// import Home from "./pages/Home";
+// import Services from "./pages/Services";
+// import Skills from "./pages/Skills";
+// import Projects from "./pages/Projects";
+// // import Main from "./components/Main";
+// // import Projects from "./components/Projects";
+// // import Resume from "./components/Resume";
+// import Contact from "./pages/Contact";
+// // import SocialLinks from "./components/SocialLinks";
+// // import Footer from "./components/Footer";
+// import "./App.css";
+
+// function App() {
+// 	return (
+// 		<div>
+// 			<Home />
+// 			<Services />
+// 			<Skills />
+// 			<Projects />
+// 			{/* <Navbar />
+// 			<Home />
+// 			<Main />
+// 			<Projects />
+// 			<Resume />
+// 			<Contact />
+// 			<SocialLinks />
+// 			<Footer /> */}
+// 			<Contact />
+// 		</div>
+// 	);
+// }
+
+// export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,26 +8,30 @@ function Home() {
 			<div className="intro">
 				<h1 className="title">I'm Rita Mwaikinda,</h1>
 				<p className="about">
-					<p>
+					<p id="aboutOne">
 						I'm a Software Developer, currently living in Miami, FL. I'm a
 						graduate of the Wyncode Coding Academy, where I developed my ability
 						to build Full Stack Web Applications, focusing primarily on the MERN
 						Stack. I love building applications, and learning new ways to make
 						technology more dynamic, every day.
 					</p>
-					<p>
+					<p className="aboutMe" id="aboutTwo">
 						<span className="bouncy">
 							I love taking on challenges and and solving problems,
 						</span>{" "}
 						with the resources available at hand. As a kid, I used to buy
 						extra-curricular homework booklets and spend my weekends gleefully
-						finding answers to problems no-one really cared about. As an adult,
-						with a Bachelors in Social Sciences and a service-driven focus, I
-						gleefully find answers to problems that can improve the lives of the
-						people using my work.
+						finding answers to problems no-one really cared about.
+						<p className="aboutMe" id="aboutThree">
+							As an adult, with a Bachelors in Social Sciences and a
+							service-driven focus, I gleefully find answers to problems that
+							can improve the lives of the people using my work.
+						</p>
 					</p>
 
-					<p>So, tell me. How can I make your day better?</p>
+					<p className="aboutMe" id="betterday">
+						So, tell me. How can I make your day better?
+					</p>
 				</p>
 				<div className="learnMore">
 					Learn More <AiOutlineDown viewBox="0 -200 1024 1024" />
