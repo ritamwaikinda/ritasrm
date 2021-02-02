15bbd92ccaffc0646fd00ce721dd61a1c8aee7cb
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-02-02 06:33:10 -0500

    resetting back to before backstage files-02

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,35 @@
 import React /*, { Component }*/ from 'react';
-import Ready from './backstagedemo/Ready.jsx';
+// import Navbar from "./components/Navbar";
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+// import Main from "./components/Main";
+// import Projects from "./components/Projects";
+// import Resume from "./components/Resume";
+import Contact from './pages/Contact';
+// import SocialLinks from "./components/SocialLinks";
+// import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
 	return (
 		<div>
-			<Ready />
+			<Home />
+			<Services />
+			<Skills />
+			<Projects />
+			{/* <Navbar />
+			<Home />
+			<Main />
+			<Projects />
+			<Resume />
+			<Contact />
+			<SocialLinks />
+			<Footer /> */}
+			<Contact />
 		</div>
 	);
 }
 
 export default App;
-
-// import React /*, { Component }*/ from "react";
-// // import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Services from "./pages/Services";
-// import Skills from "./pages/Skills";
-// import Projects from "./pages/Projects";
-// // import Main from "./components/Main";
-// // import Projects from "./components/Projects";
-// // import Resume from "./components/Resume";
-// import Contact from "./pages/Contact";
-// // import SocialLinks from "./components/SocialLinks";
-// // import Footer from "./components/Footer";
-// import "./App.css";
-
-// function App() {
-// 	return (
-// 		<div>
-// 			<Home />
-// 			<Services />
-// 			<Skills />
-// 			<Projects />
-// 			{/* <Navbar />
-// 			<Home />
-// 			<Main />
-// 			<Projects />
-// 			<Resume />
-// 			<Contact />
-// 			<SocialLinks />
-// 			<Footer /> */}
-// 			<Contact />
-// 		</div>
-// 	);
-// }
-
-// export default App;
