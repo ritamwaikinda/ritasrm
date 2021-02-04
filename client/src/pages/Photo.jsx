372121f89ca47a011dccc9de2a1b55b315e454bc
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-02-04 05:39:56 -0500

    Here I updated a lot of dependencies, trying to deploy the app

--- client/src/pages/Photo.jsx
+++ client/src/pages/Photo.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import photoOne from '../Images/photoOne.jpg';
+import photoOne from '../Images/LeftImages/photoOne.jpg';
+import photoTwo from '../Images/LeftImages/photoTwo.jpg';
+import photoThree from '../Images/LeftImages/photoThree.jpg';
 import './Photo.css';
 
 function Photo() {
 	return (
-		<div className="leftPhotos">
-			<img src={photoOne} alt="headshot of rita mwaikinda" id="photoOne" />
-			{/* <img src="#" alt="headshot of rita mwaikinda" id="photoTwo" /> */}
-			{/* <img src="#" alt="headshot of rita mwaikinda" id="photoThree" /> */}
+		<div className='leftPhotos'>
+			<img src={photoOne} alt='headshot of rita mwaikinda' id='photoOne' />
+			<img src={photoTwo} alt='headshot of rita mwaikinda' id='photoTwo' />
+			<img src={photoThree} alt='headshot of rita mwaikinda' id='photoThree' />
 		</div>
 	);
 }
